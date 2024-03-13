d903504ee18f2659692115701de3767d0bee9235
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-13 12:42:09 +0100

    needed elements got and two functions handleNextBtn and handlePrevBtn created

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Nic tu nie ma 😱')
+const formPages = document.querySelectorAll('.page');
+const steps = document.querySelectorAll('.step');
+const progressBar = document.querySelector('.progress-bar');
+const prevBtn = document.querySelector('.btn-prev');
+const nextBtn = document.querySelector('.btn-next');
+
+let currentStep = 1;
+
+const handleNextBtn = () => {
+	currentStep++;
+
+	if (currentStep > steps.length) {
+		currentStep = steps.length;
+	}
+};
+const handlePrevBtn = () => {
+	currentStep--;
+
+	if (currentStep < 1) {
+		currentStep = 1;
+	}
+};
+
+nextBtn.addEventListener('click', handleNextBtn);
+prevBtn.addEventListener('click', handlePrevBtn);
